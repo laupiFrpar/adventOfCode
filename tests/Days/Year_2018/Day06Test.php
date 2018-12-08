2c07e674f54f959baa44e1b09124f87d6d4b4586
Pierre-Louis Launay <laupi.frpar@gmail.com>
2018-12-08 17:41:30 +0100

    Add day 6

---
+++ tests/Days/Year_2018/Day06Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Lopi\AdventOfCode\Tests\Days\Year_2018;
+
+use Lopi\AdventOfCode\DayInterface;
+use Lopi\AdventOfCode\Days\Year_2018\Day06;
+use PHPUnit\Framework\TestCase;
+
+final class Day06Test extends TestCase
+{
+    protected $day;
+
+    public function __before()
+    {
+        $this->day = new Day06();
+        $this->day->setData(['1,1', '1,6', '8,3', '3,4', '5,5', '8,9']);
+    }
+    public function testPartOne(): void
+    {
+        $result = $day->getResult(DayInterface::FIRST_PART);
+
+        $this->assertEquals(17, $result[0]);
+    }
+
+    public function testPartTwo(): void
+    {
+        $this->markTestSkipped();
+
+        $result = $day->getResult(DayInterface::SECOND_PART);
+        
+        $this->assertEquals(17, $result[0]);
+    }
+}
